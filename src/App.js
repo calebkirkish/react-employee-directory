ee61c9d8010f9ecd773d7ddaa120d7cdd2e35a33
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-11-10 10:54:03 -0800

    Added a header to make it more visually appealing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import './components/table.js'
+import Header from './components/Header.js'
 import Main from './components/main.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="">
-      </header>
+      <Header />
       <Main />
     </div>
   );
