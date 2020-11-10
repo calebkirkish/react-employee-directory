192a6cdff1dcd09b64d97d572107f98941387816
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-11-10 13:33:39 -0800

    added a wrapper div

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import './components/table.js'
 import Header from './components/Header.js'
 import Main from './components/main.js';
+import Wrapper from './components/Wrapper.js';
 
 function App() {
   return (
     <div className="App">
+      <Wrapper>
       <Header />
       <Main />
+      </Wrapper>
     </div>
   );
 }
